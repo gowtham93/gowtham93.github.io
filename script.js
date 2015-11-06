5ac78193a452cb7ecb3c8a630d6678ebfbe91c63
gowtham93 <gowtham31393@gmail.com>
2015-11-06 18:48:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	$(function(){
         $("#typed").typed({
             //strings: ["Web Developer.", "WebSite Developer.", "Web App Developer.", "Explorer.", "Laugholic."],
-            strings: ["Welcome Biatchs."],
+            strings: ["Welcome to DirtyCousins Biatchs...!!"],
             typeSpeed: 50,
             backDelay: 600,
             loop: false,
