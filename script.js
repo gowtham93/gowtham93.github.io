436128f429a30dc1911226f4c653a21061c54509
gowtham93 <gowtham31393@gmail.com>
2015-11-06 16:26:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+	$(function(){
+        $("#typed").typed({
+            //strings: ["Web Developer.", "WebSite Developer.", "Web App Developer.", "Explorer.", "Laugholic."],
+            strings: ["Welcome Biatchs."],
+            typeSpeed: 50,
+            backDelay: 600,
+            loop: false,
+            contentType: 'html'
+        });
+    });
+});
+
+
+
